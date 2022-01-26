e8d2f1c0d78e74101b60f01ed49c32c5fdec45ce
gman1230321 <gshiner1@icloud.com>
2022-01-26 00:16:48 -0500

    Add authorization flow

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import aiobungie
 from os import environ
 from dotenv import load_dotenv
-from flask import Flask, request, redirect
+from flask import Flask, request
+from flask_cors import CORS
 
 baseurl = "https://localhost:5000"
 
 app = Flask(__name__)
-
-@app.route("/login")
-def login():
-    url = client.run(generate_oauth_url())
-    return redirect(url)
-
-@app.route("/authorize")
-def authorize():
-    return request.args.to_dict()
+CORS(app)
 
 load_dotenv(".env")
 
@@ -24,9 +17,29 @@ client_secret = environ["client_secret"]
 
 client = aiobungie.Client(key)
 
+@app.route("/oauth-url", methods=["GET"])
+async def getOauthUrl():
+    url = await generate_oauth_url()
+    return url
+
+@app.route("/authorize")
+async def authorizeUser():
+    async with aiobungie.RESTClient(key, client_id=int(client_id), client_secret=client_secret) as restClient:
+        code = request.args.to_dict()["code"]
+        tokens = await restClient.fetch_oauth2_tokens(code)
+        response = {
+            "access_token":tokens.access_token,
+            "refresh_token":tokens.refresh_token,
+            "expires_in":tokens.expires_in,
+            "token_type":tokens.token_type,
+            "refresh_expires_in":tokens.refresh_expires_in,
+            "membership_id":tokens.membership_id
+        }
+        return response
+
 async def generate_oauth_url() -> None:
-    async with aiobungie.RESTClient(key,client_id=client_id,client_secret=client_secret) as restClient:
+    async with aiobungie.RESTClient(key, client_id=client_id, client_secret=client_secret) as restClient:
         return restClient.build_oauth2_url()
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run()
